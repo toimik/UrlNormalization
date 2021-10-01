e04739991d41f944b5ccf0456c2a8477c00a2310
nurhafiz <nurhafiz@hotmail.sg>
2021-10-01 17:44:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,101 @@
+# Toimik.UrlNormalization
+
+.NET 5 C# [URL normalizer](https://en.wikipedia.org/wiki/URI_normalization).
+
+## Features
+
+URL normalization, also known as URL canonicalization, is the process of normalizing (standardizing) the text representation
+of a URL to determine if differently-formatted URLs are identical.
+
+#### All URLs
+
+- Duplicate slashes are removed  
+  `file://example.com/foo//bar.html` &#8594; `file://example.com/foo/bar.html`
+
+- Default port is removed  
+  `ftp://example.com:21/` &#8594; `ftp://example.com/`
+
+- Dot-segments are removed  
+  `file://example.com/foo/./bar/baz/../qux` &#8594; `file://example.com/foo/bar/qux`
+
+- Empty path is converted to "/"  
+  `ftp://example.com` &#8594; `ftp://example.com/`
+
+- Percent-encoded triplets are uppercased  
+  `ftp://example.com/foo%2a` &#8594; `ftp://example.com/foo%2A`
+
+- Percent-encoded triplets of unreserved characters are decoded  
+  `ftp://example.com/%7Efoo` &#8594; `ftp://example.com/~foo`
+
+- Scheme and host are lowercased  
+  `FTP://User@Example.COM/Foo` &#8594; `ftp://User@example.com/Foo`
+  
+#### HTTP-specific URLs
+
+- Directory index can be removed (optional, via `removableDirectoryIndexNames`)  
+  `http://example.com/default.asp` &#8594; `http://example.com/`  
+  `http://example.com/a/index.html` &#8594; `http://example.com/a/`
+
+- Fragment can be removed (optional, via `isFragmentIgnored`)
+  `http://example.com/bar.html#section1` &#8594; `http://example.com/bar.html`
+
+- Scheme can be changed (optional, via `PreferredScheme`)  
+  `https://example.com/` &#8594; `http://example.com/`
+
+- Query parameters are sorted  
+  `http://example.com/display?lang=en&article=fred` &#8594; `http://example.com/display?article=fred&lang=en`
+
+- User-info can be removed (optional, via `isUserInfoIgnored`)  
+  `http://user:password@example.com` &#8594; `http://example.com/`
+
+- Empty query is removed  
+  `http://example.com/display?` &#8594; `http://example.com/display`
+
+
+## Quick Start
+
+### Installation
+
+#### Package Manager
+
+```command
+PM> Install-Package Toimik.UrlNormalization
+```
+
+#### .NET CLI
+
+```command
+> dotnet add package Toimik.UrlNormalization
+```
+
+### Usage
+
+#### UrlNormalizer.cs
+
+```c# 
+// Use default arguments
+// var normalizer = new UrlNormalizer();
+
+// Use custom arguments
+var normalizer = new UrlNormalizer(isAdjacentSlashesCollapsed: false);
+
+var url = ...
+var normalizedlUrl = normalizer.Normalize(url);
+```
+
+#### HttpUrlNormalizer.cs
+
+```c# 
+// Use default arguments
+// var normalizer = new HttpUrlNormalizer();
+
+// Use custom arguments
+var normalizer = new HttpUrlNormalizer(
+    preferredScheme: "https",
+    isUserInfoIgnored: false,
+    removableDirectoryIndexNames: new HashSet<string>(0), // override the default
+    isFragmentIgnored: false);
+
+var url = ...
+var normalizedlUrl = normalizer.Normalize(url);
+```
---
+++ tests/Toimik.UrlNormalization.Tests/UrlNormalizerTest.cs
@@ -0,0 +1,147 @@
+﻿namespace Toimik.UrlNormalization.Tests
+{
+    using System;
+    using Xunit;
+
+    public class UrlNormalizerTest
+    {
+        [Theory]
+        [InlineData("file://example/%7Efoo", "file://example/~foo")]
+        [InlineData("ftp://example.com/%7Efoo", "ftp://example.com/~foo")]
+        public void DecodeUnreservedPercentEncodedTriplets(string url, string expectedUrl)
+        {
+            var normalizer = new UrlNormalizer();
+
+            var actualUrl = normalizer.Normalize(url);
+
+            Assert.Equal(expectedUrl, actualUrl);
+        }
+
+        [Theory]
+        [InlineData(false, "file://example/foo//bar.html", "file://example/foo//bar.html")]
+        [InlineData(true, "file://example/foo//bar.html", "file://example/foo/bar.html")]
+        [InlineData(false, "ftp://example.com/foo//bar.html", "ftp://example.com/foo//bar.html")]
+        [InlineData(true, "ftp://example.com/foo//bar.html", "ftp://example.com/foo/bar.html")]
+        public void DuplicateSlashes(
+            bool isAdjacentSlashesCollapsed,
+            string url,
+            string expectedUrl)
+        {
+            var normalizer = new UrlNormalizer(isAdjacentSlashesCollapsed: isAdjacentSlashesCollapsed);
+
+            var actualUrl = normalizer.Normalize(url);
+
+            Assert.Equal(expectedUrl, actualUrl);
+        }
+
+        [Theory]
+        [InlineData("file://", "file:///")]
+        [InlineData("file:///foo.html", "file:///foo.html")]
+        public void EmptyAuthorityThatIsNotRequired(string url, string expectedUrl)
+        {
+            var normalizer = new UrlNormalizer();
+
+            var actualUrl = normalizer.Normalize(url);
+
+            Assert.Equal(expectedUrl, actualUrl);
+        }
+
+        [Theory]
+        [InlineData("ftp://")]
+        [InlineData("ftp:///foo.html")]
+        public void EmptyAuthorityThatIsRequired(string url)
+        {
+            var normalizer = new UrlNormalizer();
+
+            Assert.Throws<UriFormatException>(() => normalizer.Normalize(url));
+        }
+
+        [Theory]
+        [InlineData("file://example", "file://example/")]
+        [InlineData("ftp://example.com", "ftp://example.com/")]
+        public void EmptyPath(string url, string expectedUrl)
+        {
+            var normalizer = new UrlNormalizer();
+
+            var actualUrl = normalizer.Normalize(url);
+
+            Assert.Equal(expectedUrl, actualUrl);
+        }
+
+        [Theory]
+        [InlineData("FILE://Example/Foo", "file://example/Foo")]
+        [InlineData("FTP://User@Example.COM/Foo", "ftp://User@example.com/Foo")]
+        public void LowercaseSchemeAndHost(string url, string expectedUrl)
+        {
+            var normalizer = new UrlNormalizer();
+
+            var actualUrl = normalizer.Normalize(url);
+
+            Assert.Equal(expectedUrl, actualUrl);
+        }
+
+        [Theory]
+        [InlineData("file://example")]
+        [InlineData("ftp://example.com")]
+        public void Relative(string baseUrl)
+        {
+            var normalizer = new UrlNormalizer();
+
+            var actualUrl = normalizer.Normalize(url: "/foo.html", baseUrl);
+
+            Assert.Equal($"{baseUrl}/foo.html", actualUrl);
+        }
+
+        [Fact]
+        public void RemoveDefaultPort()
+        {
+            var normalizer = new UrlNormalizer();
+
+            var actualUrl = normalizer.Normalize("ftp://example.com:21/");
+
+            Assert.Equal("ftp://example.com/", actualUrl);
+        }
+
+        [Theory]
+        [InlineData("file://example/foo/./bar/baz/../qux", "file://example/foo/bar/qux")]
+        [InlineData("ftp://example.com/foo/./bar/baz/../qux", "ftp://example.com/foo/bar/qux")]
+        public void RemoveDotSegments(string url, string expectedUrl)
+        {
+            var normalizer = new UrlNormalizer();
+
+            var actualUrl = normalizer.Normalize(url);
+
+            Assert.Equal(expectedUrl, actualUrl);
+        }
+
+        [Fact]
+        public void UnspecifiedSchemeWithBaseUrl()
+        {
+            var normalizer = new UrlNormalizer();
+
+            Assert.Throws<UriFormatException>(() => normalizer.Normalize("/foo", baseUrl: "example.com"));
+        }
+
+        [Fact]
+        public void UnspecifiedSchemeWithoutBaseUrl()
+        {
+            var normalizer = new UrlNormalizer();
+
+            Assert.Throws<ArgumentNullException>(() => normalizer.Normalize("example.com"));
+        }
+
+        [Theory]
+        [InlineData("file://example/foo%2a", "file://example/foo%2A")]
+        [InlineData("file://example/foo%a", "file://example/foo%25A")]
+        [InlineData("ftp://example.com/foo%2a", "ftp://example.com/foo%2A")]
+        [InlineData("ftp://example.com/foo%a", "ftp://example.com/foo%25A")]
+        public void UppercasePercentEncodedTriplets(string url, string expectedUrl)
+        {
+            var normalizer = new UrlNormalizer();
+
+            var actualUrl = normalizer.Normalize(url);
+
+            Assert.Equal(expectedUrl, actualUrl);
+        }
+    }
+}
