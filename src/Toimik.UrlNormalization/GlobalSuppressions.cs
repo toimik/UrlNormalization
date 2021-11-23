29f40c05326ef9bb6048af229b038c641ec4200a
nurhafiz <nurhafiz@hotmail.sg>
2021-11-23 15:37:35 +0800

    Configure suppression of StyleCop's SA1008 rule

---
+++ src/Toimik.UrlNormalization/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage attributes that are applied to
+// this project. Project-level suppressions either have no target or are given a specific target and
+// scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1008:Opening parenthesis should be spaced correctly", Justification = "Causes false positive for String.Substring's range operator. CodeMaid ensures that this rule is not violated.", Scope = "member", Target = "~M:Toimik.UrlNormalization.HttpUrlNormalizer.NormalizeQuery(System.String)~System.String")]
